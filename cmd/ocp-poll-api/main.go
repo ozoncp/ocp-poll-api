12a954ab4d28fda1be6e8ef735447867aae59540
Dmitriy Krayanskiy <dimkrayan@gmail.com>
2021-07-25 14:25:06 +0300

    HW3: add Poll entity and methods working with it

--- cmd/ocp-poll-api/main.go
+++ cmd/ocp-poll-api/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main()  {
+func main() {
 	fmt.Println("Hello ocp poll api")
+	for _, s := range [...]string{"File1", "File2", "/var/log/xdm.log"} {
+		openAndCloseFile(s)
+	}
+}
+
+func openAndCloseFile(fileName string) {
+	file, err := os.Open(fileName)
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			println("Unable to close file", err)
+		}
+	}()
+	if err != nil {
+		println("Unable to open file", err)
+		return
+	} else {
+		println("File exists: ", file.Name())
+	}
+
 }
