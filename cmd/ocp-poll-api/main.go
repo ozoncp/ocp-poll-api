ef9bb4614c0c2a8eb5544eaeb0a1cb0c43db264a
Dmitriy Krayanskiy <dimkrayan@gmail.com>
2021-07-28 16:53:10 +0300

    HW3: open-close file with defer - fix error handling

--- cmd/ocp-poll-api/main.go
+++ cmd/ocp-poll-api/main.go
@@ -8,23 +8,26 @@ import (
 func main() {
 	fmt.Println("Hello ocp poll api")
 	for _, s := range [...]string{"File1", "File2", "/var/log/xdm.log"} {
-		openAndCloseFile(s)
+		err := openAndCloseFile(s)
+		if err != nil {
+			return 
+		}
 	}
 }
 
-func openAndCloseFile(fileName string) {
+func openAndCloseFile(fileName string) error {
 	file, err := os.Open(fileName)
+	if err != nil {
+		return err
+	} else {
+		println("File exists: ", file.Name())
+	}
 	defer func() {
 		err := file.Close()
 		if err != nil {
 			println("Unable to close file", err)
 		}
 	}()
-	if err != nil {
-		println("Unable to open file", err)
-		return
-	} else {
-		println("File exists: ", file.Name())
-	}
+
 
 }
